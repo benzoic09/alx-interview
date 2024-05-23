63922d817f89cd93fa5f4bc30cc73a84836dbea6
Ben Kimani <benzoic09@gmail.com>
2024-05-23 13:06:15 +0000

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,39 +5,38 @@
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding"""
 
-    # Number of bytes in the current UTF-8 character
+    """Number of bytes in the current UTF-8 character"""
     num_bytes = 0
 
-    # Masks for checking the most significant bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    """Masks for checking the most significant bits"""
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
     for byte in data:
-        # Get the binary representation of the byte
-        bin_rep = byte & 0xFF  # Only consider the last 8 bits
+        bin_rep = byte & 0xFF
 
         if num_bytes == 0:
-            # Determine the number of bytes in the current UTF-8 character
+            """Determine the number of bytes in the current UTF-8 character"""
             if (bin_rep & mask1) == 0:
-                # 1-byte character (0xxxxxxx)
+                """# 1-byte character (0xxxxxxx)"""
                 continue
             elif (bin_rep & (mask1 | mask2)) == mask1:
                 return False  # Invalid leading byte
             elif (bin_rep & (mask1 | mask2)) == (mask1 | mask2):
-                # 2-byte character (110xxxxx)
+                """# 2-byte character (110xxxxx)"""
                 num_bytes = 1
             elif (bin_rep & (mask1 | mask2 | (1 << 5))) == (
                     mask1 | mask2 | (1 << 5)):
-                # 3-byte character (1110xxxx)
+                """# 3-byte character (1110xxxx)"""
                 num_bytes = 2
             elif (bin_rep & (mask1 | mask2 | (1 << 5) | (1 << 4))) == (
                     mask1 | mask2 | (1 << 5) | (1 << 4)):
-                # 4-byte character (11110xxx)
+                """# 4-byte character (11110xxx)"""
                 num_bytes = 3
             else:
-                return False  # Invalid leading byte
+                return
         else:
-            # Continuation bytes must start with 10xxxxxx
+            """# Continuation bytes must start with 10xxxxxx"""
             if (bin_rep & (mask1 | mask2)) != mask1:
                 return False
             num_bytes -= 1
